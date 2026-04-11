65df1649ba3999a969b3d3178ff28d2ac2c4870a
appprojet2-code <empire.trading.services25@gmail.com>
2026-04-11 22:24:29 +0100

    Mise à jour architecture ERP et agents

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,18 +1,1 @@
-// eslint.config.cjs
-const js = require("@eslint/js");
-const next = require("eslint-config-next");
-
-module.exports = [
-  js(),
-  ...next(),
-  {
-    ignores: [
-      "node_modules",
-      ".next",
-      "dist",
-      "build",
-      "out",
-      "schema"
-    ],
-  },
-];
+module.exports = [require('eslint-config-next')];
